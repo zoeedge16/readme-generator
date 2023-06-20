247499312470b60d9189ed6e6c8067e791c6fdcf
Zoe  Edge <zoemedge@Zoes-MacBook-Pro.local>
2023-06-19 21:15:28 -0500

    changed the format of the questions back to the OG

--- index.js
+++ index.js
@@ -3,51 +3,15 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the title of your project:'
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a description for your project:'
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter installation instructions:'
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage information:'
-      },
-      {
-        type: 'input',
-        name: 'license',
-        message: 'Enter the license for your project:'
-      },
-      {
-        type: 'input',
-        name: 'contributing',
-        message: 'Enter contribution guidelines:'
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter test instructions:'
-      },
-      {
-        type: 'input',
-        name: 'username',
-        message: 'Enter your GitHub username:'
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address:'
-      }
+    '1: Enter your project title: ',
+    '2: Enter a description of your project: ',
+    '3: Enter installation instructions: ',
+    '4: Enter usage information: ',
+    '5: Enter contribution guidelines: ',
+    '6: Enter test instructions: ',
+    '7: Enter your GitHub username: ',
+    '8: Enter your email: ',
+
 ]
 
 // TODO: Create a function to write README file
